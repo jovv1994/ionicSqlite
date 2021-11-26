3939d2d50fd49fa5734f48794bc36425a701f05d
John Vásconez <jovv1994@outlook.com>
2021-11-25 23:57:51 -0500

    Inicialización del proyecto

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sqliteIonic',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
